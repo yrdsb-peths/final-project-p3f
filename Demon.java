7f96e284dc562cdf45a907b0010fe609e2cd321f
SameerHidery <341211208@gapps.yrdsb.ca>
2022-01-23 00:17:31 -0500

    Finished foyer world (cutscene)

---
+++ Demon.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Demon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Demon extends Actor
+{
+    // A simple timer
+    private SimpleTimer timer = new SimpleTimer();
+    
+    /**
+     * Act - do whatever the Demon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setImage("Objects/Demon.png");
+        if(GameWorld.timer.millisElapsed() > 2000){
+            checkY();
+        }
+    }
+    
+    public void checkY(){
+        if(isTouching(MainCharacter.class)){
+            getWorld().removeObject(this);
+            MainCharacter.cutscene = false;
+            GameWorld.isRoomWorld = true;
+        }
+        else{
+            setLocation(getX(), getY() + deltaX(1));
+        }
+    }
+    public int deltaX(int n){
+        if(n >= 4){
+            return 0;
+        }
+        return n + deltaX(n + 1);
+    }
+}
--- GameWorld.java
+++ GameWorld.java
@@ -11,17 +11,25 @@ public class GameWorld extends World
     public static boolean isTutorialWorld1;
     public static boolean isTutorialWorld2;
     public static boolean isFoyerWorld;
+    public static boolean isRoomWorld;
+    
+    // A simple timer
+    public static SimpleTimer timer = new SimpleTimer();
     
     // Worlds
     GreenfootImage tutorialWorld1 = new GreenfootImage("Worlds/TutorialWorld1.png");
     GreenfootImage tutorialWorld2 = new GreenfootImage("Worlds/TutorialWorld2.png");
-    GreenfootImage Foyer = new GreenfootImage("Worlds/Foyer.png");
+    GreenfootImage foyerWorld = new GreenfootImage("Worlds/Foyer.png");
+    GreenfootImage roomWorld = new GreenfootImage("Worlds/Room.png");
     
-    // Create labels for instructions
+    // Create labels
     Label movement = new Label("Use W, A, S, D \n to move", 30);
     Label interact = new Label("Use E to \ninteract with\n objects", 35);
+    Label foyerDialogue1 = new Label("What the hell is that!?!?!", 30);
     
     Paper note = new Paper();
+    
+    Demon demon = new Demon();
     /**
      * Constructor for objects of class TutorialWorld.
      * 
@@ -33,6 +41,7 @@ public class GameWorld extends World
         isTutorialWorld1 = true;
         isTutorialWorld2 = false;
         isFoyerWorld = false;
+        isRoomWorld = false;
         setBackground(tutorialWorld1);
         addObject(MainCharacter.player, 300, 200);
     }
@@ -40,31 +49,38 @@ public class GameWorld extends World
     public void act(){
         changeWorlds();
         checkCrossWorlds();
-        
     }
     
     public void changeWorlds(){
-        if(isTutorialWorld1 == true){
+        if(isTutorialWorld1){
             MainCharacter.cutscene = false;
             removeObject(interact);
             removeObject(note);
             addObject(movement, 115, 110);
         }
-        else if(isTutorialWorld2 == true){
+        else if(isTutorialWorld2){
             removeObject(movement);
             addObject(note, 300, 300);
             addObject(interact, 90, 300);
             if(MainCharacter.player.getY() < 165 && Greenfoot.isKeyDown("e")){
                 isTutorialWorld2 = false;
                 isFoyerWorld = true;
+                MainCharacter.cutscene = true;
                 MainCharacter.player.setLocation(300, 320);
             }             
         }
-        if(isFoyerWorld == true){
-            MainCharacter.cutscene = true;
-            setBackground(Foyer);
+        if(isFoyerWorld){
+            isFoyerWorld = false;
+            setBackground(foyerWorld);
             removeObject(interact);
             removeObject(note);
+            addObject(demon, 300, 130);
+            addObject(foyerDialogue1, 300, 370);
+            timer.mark();
+        }
+        if(isRoomWorld){
+            removeObject(foyerDialogue1);
+            setBackground(roomWorld);
         }
     }
     
