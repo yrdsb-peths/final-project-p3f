7c62a784dd4c48ac8e1f752bef5a7bf1e1b4ecf3
SameerHidery <341211208@gapps.yrdsb.ca>
2022-01-26 16:37:12 -0500

    Comments for basket class

--- Basket.java
+++ Basket.java
@@ -1,21 +1,25 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Basket here.
+ * A basket to put certain items inside
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @@author Sameer Hidery, Murtaza Asrani, Keira Sutharsanaraj
  */
 public class Basket extends Actor
 {
+    // Sound effects
     GreenfootSound achieved = new GreenfootSound("right.mp3");
     GreenfootSound failed = new GreenfootSound("wrong.mp3");
+    
+    // booleans to determine if a bear was sewed properly
     public static boolean sewedBearFail;
     public static boolean sewedBearProperly;
     
+    // booleans to check if the correct book was found
     public static boolean bookBasketCorrect;
     public static boolean bookBasketFailed;
     
+    // Labels that tell you if you failed
     public static Label failedRoom1Task = new Label("You failed. Find 5 pieces and the needle & \n thread BEFORE interacting with the basket", 30);
     public static Label failedRoom3Task = new Label("You failed to place the correct book in the basket", 30);
     /**
@@ -29,6 +33,7 @@ public class Basket extends Actor
         checkCorrectBook();
     }
     
+    // Checks if task 1 is done right. If you have all the bear's parts with a needle and thread, sew him and put him in the basket
     public void checkSewedBear(){
         if(GameWorld.isRoom1){
             if(isTouching(MainCharacter.class) && Greenfoot.isKeyDown("e")){
@@ -37,7 +42,6 @@ public class Basket extends Actor
                     sewedBearProperly = true;
                     achieved.play();
                 }
-                
                 if(!GameWorld.pieceOne || !GameWorld.pieceTwo || !GameWorld.pieceThree || !GameWorld.pieceFour || !GameWorld.pieceFive || !GameWorld.needleAndThread){
                     sewedBearFail = true;
                     failed.play();
@@ -45,13 +49,14 @@ public class Basket extends Actor
                     game.addObject(failedRoom1Task, 300, 320);
                     GameWorld.timer.mark();
                 }
-                
             }
         }
         if(sewedBearProperly){
             setImage("Objects/BearBasket.png");
         }
     }
+
+    // Checks if task 2 is done right. If you have the correct book, put him in the basket
     public void checkCorrectBook(){
         if(GameWorld.isRoom3 && GameWorld.hallwayFirstHalfKey && isTouching(MainCharacter.class) && Greenfoot.isKeyDown("e")){
             GameWorld.room3Basket = true;
